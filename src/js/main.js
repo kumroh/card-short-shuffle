6c013e240b226a79ec81cad997adb86acf1f103f
Rohit Kumar <rohitchoudhary87@gmail.com>
2021-05-17 12:03:05 +0530

    Final version

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,30 @@
 class Card {
     constructor() {
-        const cardList = document.querySelectorAll('.card');
-        this.cards = Array.from(cardList);
-
-        const shuffleButton = document.querySelector('.shuffle');
-        shuffleButton.addEventListener('click', this.shuffle.bind(this));
-
-        const shortButton = document.querySelector('.short');
-        shortButton.addEventListener('click', this.short.bind(this));
+      const cardList = document.querySelectorAll('.card');
+      this.cards = Array.from(cardList);
+  
+      const shuffleButton = document.querySelector('.shuffle');
+      shuffleButton.addEventListener('click', this.shuffle.bind(this));
+  
+      const shortButton = document.querySelector('.short');
+      shortButton.addEventListener('click', this.short.bind(this));
     }
-
-
-
+  
+  
+  
     shuffle() {
-        const shuffledCards = this.cards.sort(() => Math.random() > Math.random() ? 1 : -1);
-        shuffledCards.forEach((card, i) => {
-            card.style.order = `${i + 1}`;
-        });
+      const shuffledCards = this.cards.sort(() => Math.random() > Math.random() ? 1 : -1);
+      shuffledCards.forEach((card, i) => {
+        card.style.order = `${i + 1}`;
+      });
     }
-
+  
     short() {
-        this.cards.forEach((card) => {
-            card.removeAttribute('style');
-        });
+      this.cards.forEach((card) => {
+        card.removeAttribute('style');
+      });
     }
-}
-
-new Card();
+  }
+  
+  new Card();
+  
